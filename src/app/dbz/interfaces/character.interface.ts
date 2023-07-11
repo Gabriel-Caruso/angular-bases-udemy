e00b58164ea10d6d7bb08acef792580b613d3d69
Gabriel Caruso <RCCARUSO93@GMAIL.COM>
2023-07-12 01:49:54 +0200

    Commit list, hero, main page and services

---
+++ src/app/dbz/interfaces/character.interface.ts
@@ -0,0 +1,8 @@
+
+
+export interface Character {
+  // TODO: Refactorizar id opcional a obligatorio. De momento se queda así para seguir adelante con el curso
+  id?: string;
+  name: string;
+  power: number;
+}
